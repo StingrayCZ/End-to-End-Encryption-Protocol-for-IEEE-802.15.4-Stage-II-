14a51b69fbe2eefc9fc932e80d78bf50cfa015d6
Jaromir Baca <47521674+StingrayCZ@users.noreply.github.com>
2020-05-29 11:10:27 +0200

    Add files via upload

---
+++ BackUP KeyEX code/Function.h
@@ -0,0 +1,40 @@
+#include <time.h>
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void ObtainPointComplex();
+
+void PointComp();
+void VerifyOfPoint();
+int LamdaSame();
+int LamdaDiff();
+int XSame();
+int XDiff();
+int YSame();
+int YDiff();
+int Recognizer();
+
+int modulo();
+int power();
+int InverseMod();
+int primeTester();
+
+void YPart();
+void XPart();
+void TheFirstPoint();
+void checkValAB();
+
+#endif // FUNCTIONS_H
+
+
+
+
+
+
+#ifndef AUX_FUNCT
+#define AUX_FUNCT
+
+#define EMPTYLINE printf("\n\r");
+
+#endif // AUX_FUNCT
