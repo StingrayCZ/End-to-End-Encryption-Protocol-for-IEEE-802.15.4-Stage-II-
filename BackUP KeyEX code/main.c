0c4dbb424079d5736256120eaf54a68585f34576
Jaromir Baca <47521674+StingrayCZ@users.noreply.github.com>
2020-05-29 03:04:15 +0200

    Add files via upload

---
+++ BackUP KeyEX code/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+
+
+int main ()
+{
+    int mod, a, b;  // pseudorandom INput
+    int X, Y, O;    // OUTput
+
+
+    ObtainPointComplex(&mod, &a, &b, &X, &Y, &O);
+
+    printf("\n\rVstupni hodnoty jsou: mod = %d, parametry jsou %d a %d", mod, a, b);
+    printf("\n\r[%d, %d], Order is %d", X, Y, O);
+
+
+
+}
